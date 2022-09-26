a9170eb49d19dff814639f90b48925d1e389b115
kei-123456789 <104549980+kei-123456789@users.noreply.github.com>
2022-09-26 14:44:32 +0900

    Create var14.php

---
+++ section5/var14.php
@@ -0,0 +1,20 @@
+<?php
+$x = 5;
+$y = 2;
+$z = $x + $y;
+echo $z;
+echo ",";
+$z = $x - $y;
+echo $z;
+echo ",";
+$z = $x * $y;
+echo $z;
+echo ",";
+$z = $x / $y;
+echo $z;
+echo ",";
+$z = $x % $y;
+echo $z;
+echo ",";
+$z = $x ** $y;
+echo $z;
