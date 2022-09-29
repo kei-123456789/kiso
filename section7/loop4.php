5c9ce830086809b5dc5a9fb2e4adefcd1122677e
kei-123456789 <104549980+kei-123456789@users.noreply.github.com>
2022-09-29 19:03:49 +0900

    Update loop4.php

--- section7/loop4.php
+++ section7/loop4.php
@@ -1,7 +1,7 @@
 <?php
-for ($i = 0; $i < 3; $i++) {
-    for ($j = 0; $j < 3; $j++) {
-        echo $i + $j;
+for ($i = 1; $i <= 9; $i++) {
+    for ($j = 1; $j <= 9; $j++) {
+        echo $i * $j;
         echo " ";
     }
     echo PHP_EOL;
