f63d383f26df1ffa93cfc0cc436629cf17f9b131
kei-123456789 <104549980+kei-123456789@users.noreply.github.com>
2022-10-04 17:30:41 +0900

    Update fs5.php

--- section13/fs5.php
+++ section13/fs5.php
@@ -1,6 +1,9 @@
 <?php
-$file = "hello.txt";
-$handle = fopen($file, "r");
+$file = "heloo.txt";
+$handle = @fopen($file, "r");
+if ($handle === false) {
+    die("can't open file");
+}
 while (($line = fgets($handle)) !== false) {
     echo $line;
 }
