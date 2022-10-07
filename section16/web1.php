5daeec7800237fc271bf440770276ccc7325bde2
kei-123456789 <104549980+kei-123456789@users.noreply.github.com>
2022-10-07 16:10:18 +0900

    Create web1.php

---
+++ section16/web1.php
@@ -0,0 +1,13 @@
+<?php
+$name = "Andy";
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP sample</title>
+</head>
+<body>
+    <h1>hello <?php echo $name; ?></h1>
+</body>
+</html>
