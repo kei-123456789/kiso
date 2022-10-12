a0e1695621135a9f82a2c4afab21177753b401ca
kei-123456789 <104549980+kei-123456789@users.noreply.github.com>
2022-10-12 16:27:58 +0900

    Create login.php

---
+++ section18/login.php
@@ -0,0 +1,9 @@
+<?php
+$id = $_POST["id"];
+$password = $_POST["password"];
+
+if ($id === "Andy" && $password === "secret") {
+    header("Location: menu.php");
+} else {
+    header("Location: login.html");
+}
