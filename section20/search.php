09e1aa58aa081e26bdc248a4ab17c305a2bfadc3
kei-123456789 <104549980+kei-123456789@users.noreply.github.com>
2022-10-17 13:53:25 +0900

    Update search.php

--- section20/search.php
+++ section20/search.php
@@ -1,13 +1,36 @@
 <?php
-$name = null;
+// $name = $_GET["name"];
+$name = (string)filter_input(INPUT_GET, "name");
 
-if (!isset($_GET["name"])) {
-    $name = null;
-} elseif (!is_string($_GET["name"])) {
-    $name = false;
-} else {
-    $name = $_GET["name"];
+$names = file("names.txt", FILE_IGNORE_NEW_LINES);
+
+$searched_names = [];
+
+if ($name != "") {
+    for ($i = 0; $i < count($names); $i++) { 
+        if (strpos($names[$i], $name) !== false) {
+            $searched_names[] = $names[$i];
+        }
+    }
 }
-
-var_dump($name);
-die("debug");
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP Sample</title>
+</head>
+<body>
+    <h3>Search</h3>
+    <hr>
+    <ul>
+        <?php
+        for ($i=0; $i < count($searched_names); $i++) { 
+        ?>
+            <li><?php echo $searched_names[$i]; ?></li>
+        <?php    
+        }
+        ?>
+    </ul>    
+</body>
+</html>
